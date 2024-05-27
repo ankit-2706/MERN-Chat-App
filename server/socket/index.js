d33413a0045897335d5461a3c8217fa8ebd3fa1d
Ankit Chouhan <coderankit27@gmail.com>
2024-05-28 02:11:59 +0530

    URL updated

--- server/socket/index.js
+++ server/socket/index.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: ["https://mern-chat-app-prl8.onrender.com"],
     credentials: true,
   },
 });
